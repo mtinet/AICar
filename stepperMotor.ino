9fea05e79acdae2a0b7b18a3dee909192c0de858
Jason Juhyun Kim <mtinet@hanmail.net>
2020-05-27 11:21:00 +0900

    Create stepperMotor.ino

---
+++ stepperMotor.ino
@@ -0,0 +1,41 @@
+const int dirPin = 2;  // Direction
+const int stepPin = 3; // Step
+ 
+// Motor steps per rotation
+const int STEPS_PER_REV = 1600;
+ 
+void setup() {
+  
+  // Setup the pins as Outputs
+  pinMode(stepPin,OUTPUT); 
+  pinMode(dirPin,OUTPUT);
+}
+void loop() {
+  
+  // Set motor direction clockwise
+  digitalWrite(dirPin,HIGH); 
+  
+  // Spin motor one rotation slowly
+  for(int x = 0; x < STEPS_PER_REV*2; x++) {
+    digitalWrite(stepPin,HIGH); 
+    delayMicroseconds(50); 
+    digitalWrite(stepPin,LOW); 
+    delayMicroseconds(50); 
+  }
+  
+  // Pause for one second
+  delay(1000); 
+    // Set motor direction clockwise
+  digitalWrite(dirPin,LOW); 
+  
+  // Spin motor one rotation slowly
+  for(int x = 0; x < STEPS_PER_REV*2; x++) {
+    digitalWrite(stepPin,HIGH); 
+    delayMicroseconds(50); 
+    digitalWrite(stepPin,LOW); 
+    delayMicroseconds(50); 
+  }
+  
+  // Pause for one second
+  delay(1000); 
+}
