8c1b822d43baf2278abaa44b1282457804302b55
Jason Juhyun Kim <mtinet@hanmail.net>
2020-06-14 14:13:06 +0900

    Update 2. accelCar.ino

--- code/2. accelCar.ino
+++ code/2. accelCar.ino
@@ -13,20 +13,19 @@
 모터 드라이버의 점퍼를 조정해 전류를 제어할 수 있음  
 모터는 K6G50C 1:50 기어박스가 포함되어 있음  
 현재 세팅은 점퍼 스위치 세팅 : 111111 (피크 전류 1A, RMS 0.71A, 400pulse/rev)  
+
 - 통신 :  
+프로그램 업로드 할 때는 반드시 블루투스 모듈을 빼놓고 해야 오류가 발생하지 않음 
+softwareSerial 라이브러리에는 parseInt 함수가 없기 때문에 하드웨어 시리얼 통신인 0번, 1번 핀을 사용해야 하기 때문
 아두이노 UNO - 블루투스 모듈(HC-06)  
-         p2 - TXD  
-         p3 - RXD  
+    0번(RX) - TXD  
+    1번(TX) - RXD  
+
 - 구동 :  
 아두이노 UNO - 모터 드라이버  
 PWM출력(p11) - PWM입력(페달)  
 전진후진(P12) - dir입력(모터 회전 방향)  
 */
-
-
-#include <SoftwareSerial.h>
-
-SoftwareSerial mySerial(2, 3); // RX, TX
 
 // 스텝 모터 제어
 const int dirPinLR = 6;  // 좌우 회전
@@ -39,13 +38,18 @@ const int STEPS_PER_REV = 400; // 모터 1회전
 const int FB = 10; // 전진, 후진
 const int PWM = 11; // 모터 회전 속도
 
-const int valocity = 100;
+// 상태 확인
+int valocity = 0; // 속도 초기화
+bool FBstate = 1; // 전진
+
+// 블루투스를 통해 들어온 값을 parseInt 함수를 사용해 x, y 값에 넣고, 이 값을 이용해 자동차를 제어함
+int x = 0;
+int y = 0;
 
 void setup() {
-  //통신 설정
-  Serial.begin(9600); // 시리얼 통신
-  mySerial.begin(9600); // 블루투스 통신             
-
+  // 통신 설정
+  Serial.begin(9600); // 시리얼 통신, 블루투스 모듈을 하드웨어 시리얼 통신 핀에 연결해서 사용함
+  
   // 스텝모터 핀 모드 설정
   pinMode(dirPinLR,OUTPUT);
   pinMode(stepPin,OUTPUT); 
@@ -55,27 +59,96 @@ void setup() {
   // 드라이브모터 핀 모드 설정
   pinMode(FB,OUTPUT);
   pinMode(PWM,OUTPUT); 
-  
+
+  // 오프닝
+  Serial.println("Now Start Receive Data by parseInt..."); 
 }
 
 void loop() {
-  if( Serial.available() ){        // 블루투스 통신에 데이터가 있을 경우
-    char cmd = Serial.read();     // 블루투스의 데이터(문자 한 글자)를 'cmd' 변수에 저장
-    if ( cmd == 'w' ){               // 만약 'cmd' 변수의 데이터가 q이면
-      forward();
-    } else if ( cmd == 'x') {        // 아니고 만약 'cmd' 변수의 데이터가 w면
-      backward();
-    } else if ( cmd == 'a' ) {       // 아니고 만약 'cmd' 변수의 데이터가 e면
-      right();
-    } else if ( cmd == 'd' ) {       // 아니고 만약 'cmd' 변수의 데이터가 e면
-      left();
-    } else if ( cmd == 's' ) {       // 아니고 만약 'cmd' 변수의 데이터가 s면
-      motorStop();
-    }
-  }
+  
 }
 
+void serialEvent(){
+  int x = Serial.parseInt();
+  int y = Serial.parseInt();
+    
+  if(Serial.read() == '\n'){
+    Serial.print("x : ");
+    Serial.print(x);
+    Serial.print("  y : ");
+    Serial.print(y);
+    Serial.print("    ");
+  }
+  moveCar(x,y);
+}
 
+void moveCar(int x, int y) {
+  
+  // 전진, 후진 제어
+  if (valocity == 0) {
+    if ( y < 13) {
+      FBstate = HIGH;
+      valocity = valocity + 10;
+    } else if ( y < 17) {
+      FBstate = HIGH;
+    } else {
+      FBstate = LOW;
+      valocity = valocity + 10;
+    }
+    Serial.println("  stop");
+    
+  } else {
+    if (FBstate == HIGH) {
+      if ( y < 13) {
+        valocity = valocity + 10;
+        valocity = constrain(valocity, 0, 255);
+        Serial.print("valocity : ");
+        Serial.print(valocity);
+        Serial.print("    forward");
+        Serial.println("  acceleration");
+      } else if ( y < 17) {
+        valocity = constrain(valocity, 0, 255);
+        Serial.print("valocity : ");
+        Serial.print(valocity);
+        Serial.print("    forward");
+        Serial.println("  stay valocity");
+      } else {
+        valocity = valocity - 10;
+        valocity = constrain(valocity, 0, 255);
+        Serial.print("valocity : ");
+        Serial.print(valocity);
+        Serial.print("    forward");
+        Serial.println("  deceleration");
+      }
+    }
+    if (FBstate == LOW) {
+      if ( y < 13) {
+        valocity = valocity - 10;
+        valocity = constrain(valocity, 0, 255);
+        Serial.print("valocity : ");
+        Serial.print(valocity);
+        Serial.print("    backward");
+        Serial.println("  deceleration");
+      } else if ( y < 17) {
+        valocity = constrain(valocity, 0, 255);
+        Serial.print("valocity : ");
+        Serial.print(valocity);
+        Serial.print("    backward");
+        Serial.println("  stay valocity");
+      } else {
+        valocity = valocity + 10;
+        valocity = constrain(valocity, 0, 255);
+        Serial.print("valocity : ");
+        Serial.print(valocity);
+        Serial.print("    backward");
+        Serial.println("  acceleration");
+      }
+    }
+  }
+  
+  analogWrite(PWM, valocity);
+  delay(100);
+}
 
 void left() {
   // 조향 모터가 '반시계방향'으로 회전하도록 신호부여
@@ -102,27 +175,4 @@ void right() {
     digitalWrite(stepPin,LOW); 
     delayMicroseconds(200); 
   }
-}
-
-void forward() {
-  //드라이브 모터가 앞으로 회전하도록 신호부여
-  digitalWrite(FB,HIGH); 
-  analogWrite(PWM, valocity);
-  delay(100);
-  Serial.println("forward");
-}
-
-void motorStop() {
-  digitalWrite(FB,HIGH); 
-  analogWrite(PWM, 0);
-  delay(100);
-  Serial.println("motorStop");
-}
-
-void backward() {
-  ////드라이브 모터가 뒤로 회전하도록 신호부여
-  digitalWrite(FB,LOW); 
-  analogWrite(PWM, valocity);
-  delay(100);
-  Serial.println("backward");
 }
