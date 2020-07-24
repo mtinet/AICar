05d94a30d4c5ed98f946eca2401b9c92dc043511
Jason Juhyun Kim <mtinet@hanmail.net>
2020-07-25 02:48:02 +0900

    Create 3. bluetooth.ino

---
+++ code/3. bluetooth.ino
@@ -0,0 +1,26 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(2, 3); // RX, TX
+
+char cmd = "";
+boolean modeState = 1;
+
+void setup() {
+  //통신 설정
+  Serial.begin(9600); // 시리얼 통신
+  mySerial.begin(9600); // 블루투스 통신   
+}
+
+void loop() {
+  if (mySerial.available() ){        // 블루투스 통신에 데이터가 있을 경우
+    cmd = mySerial.read();     // 블루투스의 데이터(문자 한 글자)를 'cmd' 변수에 저장
+  
+    // cmd 변수의 데이터가 m이면 modeState의 상태를 바꿈
+    if (cmd == 'm') {
+      modeState = !modeState;
+      Serial.println("input 'm'");
+      Serial.print("mode is : ");
+      Serial.println(modeState);
+    }
+  }
+}
