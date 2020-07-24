1a01963301b52891d2b56bc3f31e6aab5d9e9051
Jason Juhyun Kim <mtinet@hanmail.net>
2020-07-25 03:08:23 +0900

    Update and rename 1. buttonVoiceVisionCar_miniboard.ino to 0. buttonVoiceVisionCar_miniboard.ino

--- code/0. buttonVoiceVisionCar_miniboard.ino
+++ code/0. buttonVoiceVisionCar_miniboard.ino
@@ -1,4 +1,9 @@
 /* 
+*  - 통신 :  
+아두이노 UNO - 블루투스 모듈(HC-06)  
+         p2 - TXD  
+         p3 - RXD  
+         
 - 조향 :  
 아두이노 UNO - 모터 드라이버(DM322E)  
          p4 - enA 
@@ -12,21 +17,22 @@
 모터 드라이버의 점퍼를 조정해 전류를 제어할 수 있음  
 모터는 K6G50C 1:50 기어박스가 포함되어 있음  
 현재 세팅은 점퍼 스위치 세팅 : 110010 (피크 전류 3.2A, RMS 3.0A, 200pulse/rev)  
-- 통신 :  
-아두이노 UNO - 블루투스 모듈(HC-06)  
-         p2 - TXD  
-         p3 - RXD  
-- 구동 :  
-아두이노 UNO - 모터 드라이버  
-전진후진(P10) - dir입력(모터 회전 방향)  
-PWM출력(p11) - PWM입력(페달)  
-enB(p12) - 구동 여부 결정
+
 - 페달모드 :
 아두이노 - 페달
      A0 - 노랑
      7  - 스위치-전진
      8  - 스위치-후진
      9  - ground
+
+- 구동 :  
+아두이노 UNO - 모터 드라이버  
+전진후진(P10) - dir입력(모터 회전 방향)  
+PWM출력(p11) - PWM입력(파워)  
+enB(p12) - 구동 여부 결정
+
+
+- 5V 확인용 : p13
 */
 
 
@@ -35,9 +41,9 @@ enB(p12) - 구동 여부 결정
 SoftwareSerial mySerial(2, 3); // RX, TX
 
 // 스텝 모터 제어
-const int dirPinLR = 6;  // 좌우 회전
-const int stepPin = 5; // 스텝 펄스
 const int enA = 4;  // 구동 여부 결정
+const int stepPin = 5; // 스텝 펄스
+const int dirPinLR = 6;  // 좌우 회전
 
 const int STEPS_PER_REV = 200; // 모터 1회전 
 
@@ -51,7 +57,7 @@ const int vcc = 13; // 5V 확인용
 
 const int valocity = 100;
 
-// 페달 제어
+// 페달 제어_전진, 후진 스위치 센싱
 int pedalF = 7;
 boolean pedalFVal = 0;
 
@@ -60,16 +66,17 @@ boolean pedalBVal = 0;
 
 const int ground = 9;
 
+// 페달 제어_페달 센싱
 const int pedalSensor = A0;
 int pedalVal = 0;
 
 // 수동 모드, 앱 제어모드 변경, 0은 앱제어모드, 1은 수동 모드
 boolean modeState = 1;
 
-// 페달 값
+// 속도값 초기화
 int i = 0;
 
-// 입력 문자
+// 입력 문자, 입력 문자 백업
 char cmd = "";
 char cmdM = "";
 
@@ -83,7 +90,6 @@ void setup() {
   pinMode(stepPin,OUTPUT); 
   pinMode(enA, OUTPUT);
   digitalWrite(enA, HIGH);
-
 
   // 드라이브모터 핀 모드 설정
   pinMode(PWM,OUTPUT);
@@ -104,13 +110,16 @@ void setup() {
 void loop() {
   //  modestate가 1이면 페달제어 모드로 수행
   if(modeState == 1) {       
+    // 전진, 후진 스위치 값 저장
     pedalFVal = digitalRead(pedalF);
     pedalBVal = digitalRead(pedalB);
 
+    // 페달값 센싱-매핑-한계범위설정
     pedalVal = analogRead(pedalSensor);
     pedalVal = map(pedalVal, 230, 850, 0, 255);
     pedalVal = constrain(pedalVal, 0, 255);
 
+    // 페달 값 변화 시리얼 모니터링
     Serial.print(pedalFVal);
     Serial.print("  ");
     Serial.print(pedalBVal);
@@ -118,6 +127,7 @@ void loop() {
     Serial.print(pedalVal);
     Serial.print("  ");
 
+    // 페달 신호가 0이면 브레이킹
     if (pedalVal == 0) {
       digitalWrite(in1,LOW); 
       digitalWrite(in2,LOW);
@@ -125,7 +135,8 @@ void loop() {
       analogWrite(PWM, 0);
       delay(100);
     }
-    
+
+    // 전진, 후진 스위치 값에 따른 페달 동작
     if (pedalFVal == 1 && pedalBVal == 0) {
       digitalWrite(in1,HIGH); 
       digitalWrite(in2,LOW); 
@@ -144,16 +155,21 @@ void loop() {
     }
   }
          
-  if (mySerial.available() ){        // 블루투스 통신에 데이터가 있을 경우
-    cmd = mySerial.read();     // 블루투스의 데이터(문자 한 글자)를 'cmd' 변수에 저장
+  if (Serial.available() ){        // 블루투스 통신에 데이터가 있을 경우
+    cmd = Serial.read();     // 블루투스의 데이터(문자 한 글자)를 'cmd' 변수에 저장
   
-    // cmd 변수의 데이터가 m이면 modeState의 상태를 바꿈
+    // cmd 변수의 데이터가 m이면 수동모드로, i면 앱모드로 modeState 변수의 상태를 바꿈
     if (cmd == 'm') {
-      modeState = !modeState;
+      modeState = 1;
       Serial.println("input 'm'");
-      Serial.print("mode is : ");
-      Serial.println(modeState);
+      Serial.println("the mode is : manual control");
     }
+    
+    if (cmd == 'i') {
+      modeState = 0;
+      Serial.println("input 'i'");
+      Serial.println("the mode is : app control");
+    }    
     
     // modestate가 0이면 앱제어 모드로 수행
     if (modeState == 0) {
@@ -192,9 +208,9 @@ void left() {
   digitalWrite(dirPinLR,HIGH); 
   
   if (rotateLimit > 0) {
-    // 500마이크로초 주기로 모터 축이 5회전하는 코드
+    // 1000마이크로초 주기로 모터 축이 1.5회전하는 코드
     // 1:50 기어박스 내장되어 있으므로, 모터 1회전에 바퀴 7.2도 회전함
-    // 따라서, 모터가 5회전하면 바퀴가 36도 회전함
+    // 따라서, 모터가 1.5회전하면 바퀴가 10.8도 회전함
     for(int x = 0; x < STEPS_PER_REV*1.5; x++) {
       digitalWrite(stepPin,HIGH); 
       delayMicroseconds(1000); 
